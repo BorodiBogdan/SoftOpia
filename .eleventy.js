c3800fc995cfa9022fab0b430c07ec1e977116cd
SenorBorodi <77606868+SenorBorodi@users.noreply.github.com>
2022-06-05 22:52:34 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,9 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/ImperialConstruct');
     eleventyConfig.addPassthroughCopy('./src/SimplyConstruct');
     eleventyConfig.addPassthroughCopy('./src/CocktailsCrazyness');
-    eleventyConfig.addPassthroughCopy('./src/constructed');
+    eleventyConfig.addPassthroughCopy('./src/Constructed');
+    eleventyConfig.addPassthroughCopy('./src/projects.html');
+    eleventyConfig.addPassthroughCopy('./src/atlantis');
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
