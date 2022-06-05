6fba66d6eb106ae885776c6614ea08d050fd3062
SenorBorodi <77606868+SenorBorodi@users.noreply.github.com>
2022-06-05 23:08:13 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/ImperialConstruct');
     eleventyConfig.addPassthroughCopy('./src/SimplyConstruct');
     eleventyConfig.addPassthroughCopy('./src/CocktailsCrazyness');
-    eleventyConfig.addPassthroughCopy('./src/Constructed');
+    eleventyConfig.addPassthroughCopy('./src/constructed');
     eleventyConfig.addPassthroughCopy('./src/projects.html');
     eleventyConfig.addPassthroughCopy('./src/atlantis');
 
